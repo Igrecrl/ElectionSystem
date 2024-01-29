d135850847518a0689c76473efad197b73a43cdd
Igrecrl <igrecrl@hotmail.com>
2024-01-29 15:51:54 +0800

    update

---
+++ src/test/java/com/example/electionsystem/ElectionSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.electionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("test");
+    }
+
+}
